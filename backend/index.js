cf432e0b904d1b72e1567c279ad7f87753982cf3
Pranjal <pranjal7903@email.com>
2026-06-19 20:49:56 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -2,40 +2,43 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    Frontend_URL: process.env.FRONTEND_URL,
-    credentials:true
-}
 
-app.use(cors(corsOptions));
+// CORS Configuration
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://nexthire-1-zucl.onrender.com"
+    ],
+    credentials: true
+  })
+);
 
-const PORT = process.env.PORT || 8080 ;
+const PORT = process.env.PORT || 8080;
 
-
-// api's
+// Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+// Start Server
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
